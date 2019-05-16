cd946112135fc105ad39ea643f54490bc8aaf97b
Lucas Paiva da Silva <lucaspaiva.luc@gmail.com>
2019-05-16 14:24:38 -0300

    -

---
+++ AtmegaMidiTest/main.c
@@ -0,0 +1,18 @@
+/*
+ * AtmegaMidiTest.c
+ *
+ * Created: 5/16/2019 2:24:19 PM
+ * Author : Lucas
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
