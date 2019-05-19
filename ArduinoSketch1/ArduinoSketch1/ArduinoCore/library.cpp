a55e2d25b2f258fcc98ef506fbf7a047cf2785f3
Lucas Paiva da Silva <lucaspaiva.luc@gmail.com>
2019-05-19 14:32:53 -0300

    -

---
+++ ArduinoSketch1/ArduinoSketch1/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 5/18/2019 12:38:47 AM
+ * Author : Lucas
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
